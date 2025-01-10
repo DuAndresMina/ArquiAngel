a76d109fb3aa74c6b43be75b0fa6d66f5aa9ca22
Duvan Mina <129365067+DuAndresMina@users.noreply.github.com>
2025-01-10 14:11:35 +0100

    SMTP finx

---
+++ api/send-email.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+
+// Middleware para parsear datos del formulario
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Método no permitido' });
+  }
+
+  const { name, email, service, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: process.env.GMAIL_USER,
+    subject: `Consulta de ${name} - ${service}`,
+    text: `Nombre: ${name}\nEmail: ${email}\nServicio: ${service}\nMensaje: ${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return res.status(200).json({ message: 'Correo enviado correctamente.' });
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+    return res.status(500).json({ message: 'Error al enviar el correo.' });
+  }
+}
